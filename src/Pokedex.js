f612dc1a2ae4739e015280c262312334110985d1
Cleberton <clebertonfgc@gmail.com>
2021-02-11 10:49:26 -0300

    Exercicios pokedex state

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,54 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props){
+        super(props);
+
+       this.state = {
+           pokemonList: this.props.pokemons,
+           poke: 0,
+       }
+    }
+
+    nextPoke() {
+      this.state.poke+1 < this.state.pokemonList.length ?
+        this.setState((estadoAnterior, _props) => ({
+            poke: estadoAnterior.poke +1,
+        })) :
+        this.setState({
+            poke: 0,
+        })
+    }
+
+    pokeType({target}) {
+        const type = target.innerHTML;
+        const searchType = this.props.pokemons.filter((value) => value.type === type);
+
+        this.setState({
+            pokemonList: searchType,
+            poke: 0,
+        })
+    }
+
+    pokeAll() {
+        this.setState({
+            pokemonList: this.props.pokemons
+        });
+    }
+
     render() {
+        const pokemons = this.state.pokemonList;
         return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+            <div>
+                <section className="pokedex">
+                    {pokemons.filter((_poke, index) => index === this.state.poke).map((poke) => <Pokemon key={poke.id} pokemon={poke}/>)}
+                </section>
+                <section>
+                    <button onClick={ this.nextPoke.bind(this) }>Proximo Pokemon</button>
+                    <button onClick={ this.pokeType.bind(this) }>Fire</button>
+                    <button onClick={ this.pokeType.bind(this) }>Psychic</button>
+                    <button onClick={ this.pokeAll.bind(this) }>ALL</button>
+                </section>
             </div>
         );
     }
